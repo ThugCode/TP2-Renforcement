137120d627b4afa360d4f2bc6f80d4b94479cab1
Letourneur Léo <letourneur.leo@gmail.com>
2016-03-21 17:30:41 +0100

    Some random code

--- src/agent/rlagent/QLearningAgent.java
+++ src/agent/rlagent/QLearningAgent.java
@@ -2,21 +2,24 @@ package agent.rlagent;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.Map.Entry;
 
 import environnement.Action;
 import environnement.Environnement;
 import environnement.Etat;
-import environnement.gridworld.ActionGridworld;
-import environnement.gridworld.EtatGrille;
+
 /**
  * 
- * @author laetitiamatignon
+ * @author GERLAND - LETOURNEUR
  *
  */
 public class QLearningAgent extends RLAgent{
-	//TODO
 	
+	protected HashMap<Etat, HashMap<Action, Double>> Q_Values;
 	
 	/**
 	 * 
@@ -26,14 +29,11 @@ public class QLearningAgent extends RLAgent{
 	 */
 	public QLearningAgent(double alpha, double gamma,
 			Environnement _env) {
-		super(alpha, gamma,_env);
-		//TODO
 		
-	
+		super(alpha, gamma, _env);
+		
+		this.reset();
 	}
-
-
-	
 	
 	/**
 	 * renvoi la (les) action(s) de plus forte(s) valeur(s) dans l'etat e
@@ -42,10 +42,30 @@ public class QLearningAgent extends RLAgent{
 	 */
 	@Override
 	public List<Action> getPolitique(Etat e) {
-		//TODO
-		return null;
 		
+		Double maxValue = Double.MIN_VALUE;
+		ArrayList<Action> liste = new ArrayList<Action>();
 		
+		if(this.Q_Values.get(e) == null)
+			return liste;
+		
+		Iterator<Entry<Action, Double>> iterator = this.Q_Values.get(e).entrySet().iterator();
+	    while (iterator.hasNext()) {
+	    	
+	        Map.Entry<Action, Double> pair = (Map.Entry<Action, Double>)iterator.next();
+	        
+	        if(maxValue<pair.getValue()) {
+	        	maxValue = pair.getValue();
+	        	liste.clear();
+	        	liste.add(pair.getKey());
+	        }
+	        
+	        if(maxValue==pair.getValue()) {
+	        	liste.add(pair.getKey());
+	        }
+	    }
+	    
+		return liste;
 	}
 	
 	/**
@@ -53,9 +73,16 @@ public class QLearningAgent extends RLAgent{
 	 */
 	@Override
 	public double getValeur(Etat e) {
-		//TODO
-		return 0.0;
 		
+		ArrayList<Action> liste = (ArrayList<Action>) getPolitique(e);
+		if(liste.size() == 1)
+			return this.Q_Values.get(e).get(liste.get(0));
+		else if(liste.size() > 1) {
+			int i = new Random().nextInt(liste.size());
+            return this.Q_Values.get(e).get(liste.get(i));
+		}
+		
+		return 0.0;
 	}
 
 	/**
@@ -66,8 +93,11 @@ public class QLearningAgent extends RLAgent{
 	 */
 	@Override
 	public double getQValeur(Etat e, Action a) {
-		//TODO
-		return 0.0;
+		
+		if(this.Q_Values.get(e) == null || this.Q_Values.get(e).get(a) == null)
+			return 0.0;
+		
+		return this.Q_Values.get(e).get(a);
 	}
 	
 	/**
@@ -75,13 +105,22 @@ public class QLearningAgent extends RLAgent{
 	 */
 	@Override
 	public void setQValeur(Etat e, Action a, double d) {
-		//TODO
 		
+		HashMap<Action, Double> temp = new HashMap<Action, Double>();
+		temp.put(a, d);
+		
+		if(this.Q_Values.get(e) != null) {
+			this.Q_Values.get(e).put(a, d);
+			this.Q_Values.replace(e, this.Q_Values.get(e));
+		}
+		else {
+			this.Q_Values.put(e, temp);
+		}
 		
 		//mise a jour vmin et vmax pour affichage gradient de couleur
-		//...
-		
-		
+        Double value = this.Q_Values.get(e).get(a);
+    	vmax = Math.max(value, vmax);
+    	vmin = Math.min(value, vmin);
 		
 		this.notifyObs();
 	}
@@ -98,8 +137,10 @@ public class QLearningAgent extends RLAgent{
 	 */
 	@Override
 	public void endStep(Etat e, Action a, Etat esuivant, double reward) {
-		//TODO
 		
+		Double meilleur = this.getValeur(esuivant);
+		Double valeur = (1-this.alpha)*this.getQValeur(e,a)+this.alpha*(reward+this.gamma*meilleur);
+		this.setQValeur(e, a, valeur);
 	}
 
 	@Override
@@ -115,20 +156,11 @@ public class QLearningAgent extends RLAgent{
 	public void reset() {
 		super.reset();
 		this.episodeNb =0;
-		//TODO
-		
+
+		this.vmax = 0;
+        this.vmin = Integer.MAX_VALUE;
+		this.Q_Values = new HashMap<Etat, HashMap<Action, Double>>();
 		
 		this.notifyObs();
 	}
-
-
-
-
-	
-
-
-
-	
-
-
 }
